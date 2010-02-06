31ea19043b9da857ed379f1d877944d3b43e02cc
arrai <array.of.intellect@gmail.com>
2010-02-06 14:18:37 +0100

    fixed batch for multiple harddrive cases

--- src/decrypter/decrypter.bat
+++ src/decrypter/decrypter.bat
@@ -1,5 +1,10 @@
 @echo off
 echo "Tiawps decrypter"
-cd %~dp0
+
+SET PATH=%~dp0
+REM Switch to drive
+%PATH:~0,2%
+
+cd %PATH%
 tiawps_decrypter.exe %*
 PAUSE
--- src/reader/reader.bat
+++ src/reader/reader.bat
@@ -1,5 +1,11 @@
 @echo off
 echo "Tiawps sessionkey reader Build 11403 3.3.2"
-cd %~dp0
+
+SET PATH=%~dp0
+REM Switch to drive
+%PATH:~0,2%
+
+cd %PATH%
+
 tiawps_reader.exe 13186064 1288
 PAUSE
