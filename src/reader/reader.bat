4a4b78bce4980bc7da3b153303cad59ffb13b121
sniper231 <sniper231@users.github.com>
2010-02-06 13:47:13 +0100

    changed offset for 3.3.2

--- src/reader/reader.bat
+++ src/reader/reader.bat
@@ -1,5 +1,5 @@
 @echo off
-echo "Tiawps sessionkey reader Build 11159 3.3.x"
+echo "Tiawps sessionkey reader Build 11403 3.3.2"
 cd %~dp0
-tiawps_reader.exe 13181888 1288
+tiawps_reader.exe 13186064 1288
 PAUSE
