01928e8a96001287e6e4896aa534e2d595ae9b86
arrai <array.of.intellect@gmail.com>
2010-03-11 15:46:47 +0100

    add client build to sqlite output

--- src/decrypter/sqliteout.c
+++ src/decrypter/sqliteout.c
@@ -5,6 +5,18 @@
 #include "tools.h"
 
 
+void executeSql(sqlite3 *db, const char* cmd)
+{
+    int rc;
+    char *errMsg = 0;
+    rc = sqlite3_exec(db, cmd, NULL, 0, &errMsg);
+    if( rc!=SQLITE_OK ){
+        printf("SQL error: %s\n", errMsg);
+        sqlite3_free(errMsg);
+        exit(1);
+    }
+}
+
 void initDatabase(const char* filename, sqlite3 **db)
 {
     int rc = sqlite3_open(filename, db);
@@ -14,35 +26,40 @@ void initDatabase(const char* filename, sqlite3 **db)
         exit(1);
     }
     printf("opened database %s\n",filename);
-    char *errMsg = 0;
-    rc = sqlite3_exec(*db, "create table packets (id integer primary key autoincrement, timestamp datetime, direction integer, opcode integer, data blob);", NULL, 0, &errMsg);
+    executeSql(*db, "create table packets (id integer primary key autoincrement, timestamp datetime, direction integer, opcode integer, data blob);");
 
-    if( rc!=SQLITE_OK ){
-        printf("SQL error: %s\n", errMsg);
-        sqlite3_free(errMsg);
-        exit(1);
-    }
-    rc = sqlite3_exec(*db, "BEGIN;", NULL, 0, &errMsg);
+    executeSql(*db, "BEGIN;");
 
-    if( rc!=SQLITE_OK ){
-        printf("SQL error: %s\n", errMsg);
-        sqlite3_free(errMsg);
-        exit(1);
-    }
-
+    executeSql(*db, "create table header (`key` string primary key, value string);");
 }
 
 void freeDatabase(sqlite3 **db)
 {
-    char *errMsg = 0;
-    int rc = sqlite3_exec(*db, "COMMIT;", NULL, 0, &errMsg);
+    executeSql(*db, "COMMIT;");
 
-    if( rc!=SQLITE_OK ){
-        printf("SQL error: %s\n", errMsg);
-        sqlite3_free(errMsg);
+    sqlite3_close(*db);
+}
+
+void insertClientBuild(uint8_t *data, uint32_t data_len, sqlite3 *db)
+{
+    if(data_len <4)
+    {
+        printf("FATAL: got a packet with opcode = 493 = CMSG_AUTH_SESSION but payload len=%u < 4\n", data_len);
         exit(1);
     }
-    sqlite3_close(*db);
+    uint32_t *clientBuild = (uint32_t*)data;
+
+    const char* insertFormat = "insert into header values ('clientBuild', %u)";
+    uint32_t allocSize = strlen(insertFormat)+10 /*uint32*/;
+    char* buffer = malloc(allocSize);
+    if(!buffer)
+    {
+        printf("Failed to allocate %u bytes for insert query\n", allocSize);
+        exit(1);
+    }
+    sprintf(buffer, insertFormat, *clientBuild);
+    executeSql(db, buffer);
+    free(buffer);
 }
 
 void insertPacket(uint8_t s2c, uint64_t time, uint16_t opcode, uint8_t *data, uint32_t data_len, void* arg)
@@ -62,14 +79,11 @@ void insertPacket(uint8_t s2c, uint64_t time, uint16_t opcode, uint8_t *data, ui
 
     sprintf(queryBuffer, insertFormat, (uint32_t)(time/1000000), s2c, opcode, hexEncode(data, data_len));
 
-    char *errMsg = 0;
-    int rc = sqlite3_exec(db, queryBuffer, NULL, 0, &errMsg);
+    executeSql(db, queryBuffer);
 
     free(queryBuffer);
-    if( rc!=SQLITE_OK ){
-        printf("SQL error: %s\n", errMsg);
-        sqlite3_free(errMsg);
-        exit(1);
-    }
 
+    if(opcode == 493)
+        insertClientBuild(data, data_len, db);
 }
+
